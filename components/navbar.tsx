65072f811101d9d2b5ae4f4341ccfbc0061edf0d
Hüseyin Tunç <huseyint428@gmail.com>
2025-03-31 22:07:46 +0300

    drop search input

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -102,17 +102,13 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
+        
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
               <Link
                 color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
+                  "primary"
                 }
                 href={item.href}
                 size="lg"
