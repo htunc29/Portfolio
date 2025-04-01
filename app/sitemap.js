e1bd9ed9a14afd251579d596b2f2de734596373a
Hüseyin Tunç <huseyint428@gmail.com>
2025-04-01 11:09:31 +0300

    add sitemap.xml

---
+++ app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap(){
+    return[
+            {
+            url: "https://tuncyazilim.web.tr",
+            lastModified: new Date().toISOString(),
+          },
+          {
+            url:"https://tuncyazilim.web.tr/about",
+            lastModified: new Date().toISOString(),
+          },
+          {
+            url: "https://tuncyazilim.web.tr/contact",
+            lastModified: new Date().toISOString(),
+          },
+    ]
+}
