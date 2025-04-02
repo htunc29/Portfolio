2faa220c1455c34527833689c59ab682393231d3
Hüseyin Tunç <huseyint428@gmail.com>
2025-04-02 14:29:52 +0300

    Revert "update sitemap.xml"
    This reverts commit 6067cf9adebae1110c4f8d8988dc06df20bfda74.

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,20 +1,16 @@
-export default function sitemap() {
-    return [
-      {
-        url: "https://tuncyazilim.web.tr",
-        lastModified: new Date().toISOString(),
-        priority: 1.0,
-      },
-      {
-        url: "https://tuncyazilim.web.tr/about",
-        lastModified: new Date().toISOString(),
-        priority: 0.8,
-      },
-      {
-        url: "https://tuncyazilim.web.tr/contact",
-        lastModified: new Date().toISOString(),
-        priority: 0.7,
-      },
-    ];
-  }
-  
+export default function sitemap(){
+    return[
+            {
+            url: "https://tuncyazilim.web.tr",
+            lastModified: new Date().toISOString(),
+          },
+          {
+            url:"https://tuncyazilim.web.tr/about",
+            lastModified: new Date().toISOString(),
+          },
+          {
+            url: "https://tuncyazilim.web.tr/contact",
+            lastModified: new Date().toISOString(),
+          },
+    ]
+}
